d60d3a4383cc1929c2e9d7a930b430226eeb42f8
Shiva Aleti <aletishiva218@gmail.com>
2024-08-19 10:48:33 +0530

    added json object data

--- route/uploadImage.js
+++ route/uploadImage.js
@@ -2,7 +2,7 @@ const uploadImg = (req,res) => {
   let data = req.body.toString('base64');
   console.log(data)
     data = "data:image/png;base64,"+data;
-    res.send(data);
+    res.json({status:"Ok",url:data});
   }
 
   export default uploadImg;
